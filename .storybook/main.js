9e045391e9b1e8a1138c84cdfec39f7a64ebbfc0
kaori-igawa <kaori0v0igawa@gmail.com>
2025-06-06 16:45:58 +0900

    update: storybookのバージョン変更

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,11 +7,15 @@ const config = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
+    {
+      "name": "@storybook/addon-essentials",
+      "options": {
+        "docs": false
+      }
+    },
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-interactions"
   ],
   "framework": {
     "name": "@storybook/react-vite",
